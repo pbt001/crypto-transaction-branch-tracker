60ce3561aa16e3e1261be7bacdeabf295d4c5135
Jack Hickey <jack.hickey@outlook.com>
2020-06-03 19:16:10 +0100

    Built a super basic starting point for the actual control that will be holding a branch: ctrlBranch

---
+++ Crypto Transaction Branch Tracker/CryptoBranchTracker.WFA/Controls/ctrlBranch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CryptoBranchTracker.WFA.Classes;
+using System.Reflection.Emit;
+using System.Configuration;
+
+namespace CryptoBranchTracker.WFA.Controls
+{
+    public partial class ctrlBranch : GroupBox
+    {
+        public Branch Branch { get; set; } = new Branch();
+
+        public ctrlBranch()
+        {
+            InitializeComponent();
+        }
+
+        private void RefreshDetails()
+        {
+            try
+            {
+                //Don't want to be dealing with totally empty branches really
+                if (this.Branch.Identifier != Guid.Empty)
+                {
+                    this.Text = this.Branch.DateCreated.HasValue ? this.Branch.DateCreated.Value.ToLongDateString() : "Unknown";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred refreshing details: {ex}");
+            }
+        }
+
+        public ctrlBranch(Branch branch)
+        {
+            try
+            {
+                this.Branch = branch;
+                this.RefreshDetails();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred initializing the branch control: {ex}");
+            }
+        }
+    }
+}
--- Crypto Transaction Branch Tracker/CryptoBranchTracker.WFA/Forms/frmMain.cs
+++ Crypto Transaction Branch Tracker/CryptoBranchTracker.WFA/Forms/frmMain.cs
@@ -1,4 +1,5 @@
 ﻿using CryptoBranchTracker.WFA.Classes;
+using CryptoBranchTracker.WFA.Controls;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -22,14 +23,26 @@ namespace CryptoBranchTracker.WFA.Forms
 
         private void frmMain_Load(object sender, EventArgs e)
         {
-            
+            try
+            {
+                List<Branch> lstBranches = Branch.GetAllLocalBranches();
+
+                foreach (Branch branch in lstBranches)
+                {
+                    this.flpContainer.Controls.Add(new ctrlBranch(branch));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void btnNewBranch_Click(object sender, EventArgs e)
         {
             try
             {
-
+                new Branch() { DateCreated = DateTime.Now.Date }.Save();
             }
             catch (Exception ex)
             {
