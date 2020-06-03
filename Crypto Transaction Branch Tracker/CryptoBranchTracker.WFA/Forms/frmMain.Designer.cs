c66018c2ed482404657fe3a9d811054e6b78d431
Jack Hickey <jack.hickey@outlook.com>
2020-06-03 15:40:12 +0100

    Set Forms/frmMain.cs as startup form

--- Crypto Transaction Branch Tracker/CryptoBranchTracker.WFA/Forms/frmMain.Designer.cs
+++ Crypto Transaction Branch Tracker/CryptoBranchTracker.WFA/Forms/frmMain.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace CryptoBranchTracker.WFA
+﻿namespace CryptoBranchTracker.WFA.Forms
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -31,10 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "frmMain";
         }
 
         #endregion
     }
 }
-
