b5406eb78d30c9c083b509cea16700c6a1e1e0e2
Jack Hickey <jack.hickey@outlook.com>
2020-06-04 14:16:53 +0100

    Any windows will now inherit from Classes/BaseWindow.cs, as wndMain.xaml currently does

---
+++ Crypto Transaction Branch Tracker/CryptoBranchTracker.WPF/Classes/BaseWindow.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace CryptoBranchTracker.WPF.Classes
+{
+    public class BaseWindow : Window
+    {
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        private const int GWL_STYLE = -16;
+
+        private const int WS_MAXIMIZEBOX = 0x10000;
+        private const int WS_MINIMIZEBOX = 0x20000;
+
+        public BaseWindow()
+        {
+            try
+            {
+                this.SourceInitialized += MainWindow_SourceInitialized;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred initializing the window: {ex}");
+            }
+        }
+
+        private IntPtr _windowHandle;
+        private void MainWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            _windowHandle = new WindowInteropHelper(this).Handle;
+
+            HideMinimizeAndMaximizeButtons();
+        }
+
+        protected void HideMinimizeAndMaximizeButtons()
+        {
+            if (_windowHandle == null)
+                throw new InvalidOperationException("The window has not yet been completely initialized");
+
+            SetWindowLong(_windowHandle, GWL_STYLE, GetWindowLong(_windowHandle, GWL_STYLE) & ~WS_MAXIMIZEBOX & ~WS_MINIMIZEBOX);
+        }
+
+        protected void DisableMinimizeButton()
+        {
+            if (_windowHandle == IntPtr.Zero)
+                throw new InvalidOperationException("The window has not yet been completely initialized");
+
+            SetWindowLong(_windowHandle, GWL_STYLE, GetWindowLong(_windowHandle, GWL_STYLE) & ~WS_MINIMIZEBOX);
+        }
+    }
+}
--- Crypto Transaction Branch Tracker/CryptoBranchTracker.WPF/Windows/wndMain.xaml.cs
+++ Crypto Transaction Branch Tracker/CryptoBranchTracker.WPF/Windows/wndMain.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CryptoBranchTracker.WPF.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,11 +18,107 @@ namespace CryptoBranchTracker.WPF.Windows
     /// <summary>
     /// Interaction logic for wndMain.xaml
     /// </summary>
-    public partial class wndMain : Window
+    public partial class wndMain : BaseWindow
     {
         public wndMain()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Check the state of the window to determine the visibility of the maximize/restore title bar buttons
+        /// </summary>
+        private void CheckMaxRestore()
+        {
+            try
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.btnMaximize.Visibility = Visibility.Collapsed;
+                    this.btnRestore.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    this.btnMaximize.Visibility = Visibility.Visible;
+                    this.btnRestore.Visibility = Visibility.Collapsed;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred checking max/restore state: {ex}");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SystemCommands.MinimizeWindow(this);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SystemCommands.CloseWindow(this);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SystemCommands.MaximizeWindow(this);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SystemCommands.RestoreWindow(this);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BaseWindow_StateChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                this.CheckMaxRestore();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BaseWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.CheckMaxRestore();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
